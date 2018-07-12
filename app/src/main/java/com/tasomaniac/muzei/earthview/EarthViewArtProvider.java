6a167c85edc0eafed61da98602c31f68f9e00514
Said Tahsin Dane <tasomaniac@gmail.com>
2018-07-12 22:36:17 +0200

    add provider

---
+++ app/src/main/java/com/tasomaniac/muzei/earthview/EarthViewArtProvider.java
@@ -0,0 +1,10 @@
+package com.tasomaniac.muzei.earthview;
+
+import com.google.android.apps.muzei.api.provider.MuzeiArtProvider;
+
+public class EarthViewArtProvider extends MuzeiArtProvider {
+
+    @Override protected void onLoadRequested(boolean initial) {
+
+    }
+}
